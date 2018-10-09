8c635b1a7ca109778f5a85137f4bb9f04c234291
Student <student@imac-08.local>
2018-10-09 12:30:41 +0200

    almost done

--- lab2ml/ViewController.swift
+++ lab2ml/ViewController.swift
@@ -10,11 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var date: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        date.text = "2323232"
     }
-
-
 }
 
