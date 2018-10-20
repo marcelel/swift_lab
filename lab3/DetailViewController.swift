ea23e5007f1c20a2b4ba45a54f22ae82c1bf85f4
Amelia Lekston <amelialekston@macbook-air-amelia.local>
2018-10-20 21:46:05 +0200

    Initial Commit

---
+++ lab3/DetailViewController.swift
@@ -0,0 +1,40 @@
+//
+//  DetailViewController.swift
+//  lab3
+//
+//  Created by Amelia Lekston on 20/10/2018.
+//  Copyright © 2018 Marcel Lekston. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = detailItem {
+            if let label = detailDescriptionLabel {
+                label.text = detail.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        configureView()
+    }
+
+    var detailItem: NSDate? {
+        didSet {
+            // Update the view.
+            configureView()
+        }
+    }
+
+
+}
+
